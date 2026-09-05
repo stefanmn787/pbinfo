9cc7c60a25e1af3700b9695065194571f8015b6a
stefanmn787 <stefanmonu2@gmail.com>
2026-09-05 20:20:52 +0300

    p1260 solved 100p

--- p941/main.cpp
+++ p941/main.cpp
@@ -6,6 +6,9 @@ int main()
 {
     int a, b;
     cin >> a >> b;
-    cout << a-b ;
+    cout << a+b << " ";
+    cout << a-b << " ";
+    cout << a*b << " ";
+    cout << a/b << " ";
     return 0;
 }
