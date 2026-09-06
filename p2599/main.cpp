309e386bd38aee3fa056ec7d4cb6db89f3a9aef9
stefanmn787 <stefanmonu2@gmail.com>
2026-09-06 17:51:41 +0300

    p2377 solved 100p

--- p2599/main.cpp
+++ p2599/main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a,a2,a4,a8,a16;
+    cin>>a;
+    a2=a*a;
+    a4=a2*a2;
+    a8=a4*a4;
+    a16=a8*a8;
+    cout<<a16;
+    return 0;
+
     return 0;
 }
