4d8592ef808508ccc57dc8a2e3e258b185ed50d0
stefanmn787 <stefanmonu2@gmail.com>
2026-09-09 22:25:01 +0300

    p1892 solved 100p

--- p1360/main.cpp
+++ p1360/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    int n,s;
+int main() {
+    int n;
     cin >> n;
-    s=( n * ( n + 1 ) ) / 2;
-    cout << s;
+
+    if (n % 2 == 0) {
+        cout << "NU ESTE NOROCOS";
+    } else {
+        int primul = (n + 1) / 2;
+
+        for (int i = 0; i < n; i++) {
+            cout << primul + i;
+            if (i < n - 1)
+                cout << ' ';
+        }
+    }
+
     return 0;
 }
