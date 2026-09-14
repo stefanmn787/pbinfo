15811335a252cf2d658aa9937761c5fb98c3e8a1
stefanmn787 <stefanmonu2@gmail.com>
2026-09-14 22:34:25 +0300

    p2764 solved 100p

--- p4185/main.cpp
+++ p4185/main.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main()
 {
-    int a,b,c,d;
-    cin>>a>>b>>c>>d;
-    cout<<(c*b-d*a)/(d-c);
+    double a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+	long double result = (c * b - d * a) / (d - c);
+
+    long long doublePart = (long long)(result * 10) % 10 << (long long)(result * 100) % 10;
+
+    cout << (long long)(result) << "." << (abs)((long long)(result * 10) % 10) << (abs)((long long)(result * 100) % 10);
+
     return 0;
 }
