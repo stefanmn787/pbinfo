5ecd78b9a6cd16c882674a2cc1836f42428d1296
stefanmn787 <stefanmonu2@gmail.com>
2026-09-14 22:37:02 +0300

    p1343 solved 100p

--- p2764/main.cpp
+++ p2764/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int r,g,b;
-    cin >> r >> g >>b;
-    cout<< 255 - r << ' ' << 255 - g << ' ' << 255 - b;
+    int n;
+    cin >> n;
+    cout << sqrt(n);
     return 0;
 }
