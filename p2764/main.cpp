40c4145f9fb2fe9fda2ab449df8899c50b5aa648
stefanmn787 <stefanmonu2@gmail.com>
2026-09-14 22:43:36 +0300

    p2602 solved 100p

--- p2764/main.cpp
+++ p2764/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << sqrt(n);
+    double a;
+    cin >> a;
+    int n = (int)a;
+    cout << n % 10;
     return 0;
 }
