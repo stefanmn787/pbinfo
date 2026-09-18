c29ebec26c24c19587f45a2f81c9c3d4eb89b724
stefanmn787 <stefanmonu2@gmail.com>
2026-09-18 21:46:49 +0300

    p109 solved 100p

---
+++ p109/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n % 2 == 0)
+    {
+        cout << n << " este par";
+    }
+    else cout << n << " este impar";
+    return 0;
+}
